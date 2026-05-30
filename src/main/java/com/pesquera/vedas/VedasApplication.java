d9e498dc5b946043f76f0c83c1a1ce4cdb5d93e0
eidan473 <eidanc1596.com>
2026-05-29 21:13:47 -0400

    feat(ms-vedas): entidades, repositorio, service, dto y controller

---
+++ src/main/java/com/pesquera/vedas/VedasApplication.java
@@ -0,0 +1,13 @@
+package com.pesquera.vedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VedasApplication.class, args);
+	}
+
+}
